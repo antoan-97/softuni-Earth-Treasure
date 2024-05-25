d1ae4641c0b2edec98812298c98b27956c1aa656
antoan-97 <antoan.todorov@abv.bg>
2024-05-25 15:13:02 +0300

    render stones in homePage

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,14 @@
+const Stones = require('../models/Stones');
+
 const router = require('express').Router();
 
-router.get('/', (req,res) =>{
-    res.render('home')
+router.get('/', async (req,res) =>{
+    const stones = await Stones.find()
+    res.render('home', { stones });
 });
 
 router.get('/404', (req,res) =>{
-    res.render('404')
+    res.render('404');
 })
 
 module.exports = router;
