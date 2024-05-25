8abf5c523e16451fdff00f65b5dfd7f84ce4eb64
antoan-97 <antoan.todorov@abv.bg>
2024-05-25 13:20:11 +0300

    add schema validation and error messages

--- src/models/Stones.js
+++ src/models/Stones.js
@@ -4,26 +4,39 @@ const stonesSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true,
+        minLength: [2, 'Name should be at least 2 characters long!'],
+    },
+    color:{
+        type:String,
+        required:true,
+        minLength: [2, 'Color should be at least 2 characters long!'],
     },
     category:{
         type:String,
         required:true,
+        minLength: [3, 'Category should be at least 3 characters long!'],
     },
     image:{
         type:String,
         required:true,
+        match:[/^https?:\/\//, "Invalid URL!"]
     },
     location:{
         type:String,
         required:true,
+        minLength: [5, 'Location should be be between 5 and 15 characters long!'],
+        maxLength: [15,'Location should be be between 5 and 15 characters long!'],
     },
     formula:{
         type:String,
         required:true,
+        minLength: [3, 'Formula should be be between 3 and 30 characters long!'],
+        maxLength: [30,'Formula should be be between 3 and 30 characters long!'],
     },
     description:{
         type:String,
         required:true,
+        minLength: [10, 'Description should be at least 10 characters long!'],
     },
     likedList:[
         {
