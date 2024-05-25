79aa18b90516c046fe7eed20931ba0de49e732f5
antoan-97 <antoan.todorov@abv.bg>
2024-05-25 12:18:13 +0300

    add create controller

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const userController = require('./controllers/userController');
+const stoneController = require('./controllers/stoneController');
 
 
 
 router.use(homeController);
 router.use('/users',userController);
+router.use('/stones', stoneController);
 
 
 module.exports = router
