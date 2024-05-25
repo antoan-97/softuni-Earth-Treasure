a45fbeedf212ff7beea065d7605c711722b4b2ae
antoan-97 <antoan.todorov@abv.bg>
2024-05-25 13:59:11 +0300

    create getOne manager function

--- src/managers/stoneManager.js
+++ src/managers/stoneManager.js
@@ -3,4 +3,6 @@ const Stones = require('../models/Stones');
 
 exports.getAll = () => Stones.find().populate('owner')
 
+exports.getOne = (stoneId) => Stones.findById(stoneId).populate('owner');
+
 exports.create = (stoneData) => Stones.create(stoneData);
