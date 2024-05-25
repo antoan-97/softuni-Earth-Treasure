9c320f0a11e5a95f260fa3b5003d25544b94e8a2
antoan-97 <antoan.todorov@abv.bg>
2024-05-25 12:36:10 +0300

    create router for dashboard page

--- src/controllers/stoneController.js
+++ src/controllers/stoneController.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 
+router.get('/', (req,res) =>{
+    res.render('stones')
+});
+
 router.get('/create' , (req,res) =>{
     res.render('stones/create')
-})
+});
+
 
 
 module.exports = router;
