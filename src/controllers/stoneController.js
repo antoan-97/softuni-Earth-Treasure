b1df851b2f041a7bef90efccca163ffc14c2803f
antoan-97 <antoan.todorov@abv.bg>
2024-05-25 13:41:12 +0300

    add create functionality

--- src/controllers/stoneController.js
+++ src/controllers/stoneController.js
@@ -1,12 +1,31 @@
 const router = require('express').Router();
+const { getErrorMessage } = require('../utils/errorHelper');
 
-router.get('/', async (req,res) =>{
-    const photos = await stoneManager.getAll().lean();
-    res.render('stones')
+const stoneManager = require('../managers/stoneManager');
+
+router.get('/', async (req, res) => {
+    const stones = await stoneManager.getAll().lean();
+    res.render('stones', { stones })
 });
 
-router.get('/create' , (req,res) =>{
+router.get('/create', (req, res) => {
     res.render('stones/create')
+});
+
+
+router.post('/create', async (req, res) => {
+    const stoneData = {
+        ...req.body,
+        owner: req.user._id,
+    };
+
+    try {
+        await stoneManager.create(stoneData);
+        res.redirect('/stones')
+    } catch (err) {
+        res.render('stones/create', { error: getErrorMessage(err) })
+    }
+    
 });
 
 
