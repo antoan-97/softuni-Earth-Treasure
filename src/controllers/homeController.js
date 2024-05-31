394e74995e36daa3b143a972c52b38775aa2de2d
antoan-97 <antoan.todorov@abv.bg>
2024-05-31 12:45:23 +0300

    fix home page stones rendering
    show only last 3 added stones in home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,7 +3,7 @@ const Stones = require('../models/Stones');
 const router = require('express').Router();
 
 router.get('/', async (req,res) =>{
-    const stones = await Stones.find()
+    const stones = await Stones.find().sort({ _id: -1 }).limit(3);
     res.render('home', { stones });
 });
 
