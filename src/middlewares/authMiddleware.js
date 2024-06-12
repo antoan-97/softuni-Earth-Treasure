9620d2c868af9939699a7c75eeb7b78da7bcd8c7
antoan-97 <antoan.todorov@abv.bg>
2024-06-12 14:44:16 +0300

    create more middlewares

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require('../lib/jwt');
 const { SECRET } = require('../config/config');
+const stoneManager = require('../managers/stoneManager');
 
 
 exports.auth = async (req,res,next) => {
@@ -25,6 +26,24 @@ exports.auth = async (req,res,next) => {
 exports.isAuth = (req,res,next) =>{
     if(!req.user){
         res.redirect('/users/login');
+    }
+    next();
+};
+
+exports.isLoggedIn = (req, res, next) => {
+    if (req.user) {
+        // Redirect to 404 page  if user is already logged in
+        return res.render('404');
+    }
+    next();
+};
+
+
+exports.isOwner = async (req, res, next) => {
+    const stoneId = req.params.stoneId;
+    const stone = await stoneManager.getOne(recipeId);
+    if (!stone || stone.owner.toString() !== req.user._id.toString()) {
+        return res.render('404');
     }
     next();
 };
